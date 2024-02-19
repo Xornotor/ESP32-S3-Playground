b190650125aa70b0d35863225a86083ab7f13a76
And Paiva <andre.pcr@hotmail.com>
2024-02-19 13:35:08 -0300

    Removed unused directories

--- RGB-LED/src/main.c
+++ RGB-LED/src/main.c
@@ -7,24 +7,28 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-uint8_t color_weight = 74;
-unsigned int color = 0x000000;
-uint32_t color_duties[75];
-const uint32_t freq_hz = 40000;
-const uint8_t duty_resolution = 7;
-const double duty_unit = (1e9/(freq_hz * 128));
+#define FREQ_HZ         40000
+#define DUTY_RESOLUTION 7
+const double duty_unit = (1e9/(FREQ_HZ * 128));
 const uint32_t LOW_DCYCLE = (uint32_t)(300/duty_unit);
 const uint32_t HIGH_DCYCLE = (uint32_t)(700/duty_unit);
+
+uint8_t color_weight = 74;
+unsigned int color = 0x000001;
+uint32_t color_duties[75];
+uint8_t color_rainbow_state = 0;
 
 static bool change_color(gptimer_handle_t, const gptimer_alarm_event_data_t*, void*);
 void init_led();
 void set_color(unsigned int);
+void color_shift();
+void color_rainbow();
 
 const ledc_timer_config_t led_timer_config = {
     .speed_mode = LEDC_LOW_SPEED_MODE,
-    .duty_resolution = duty_resolution,
+    .duty_resolution = DUTY_RESOLUTION,
     .timer_num = LEDC_TIMER_0,
-    .freq_hz = freq_hz,
+    .freq_hz = FREQ_HZ,
     .clk_cfg = LEDC_AUTO_CLK
 };
 
@@ -43,7 +47,7 @@ gptimer_handle_t color_timer_handler = NULL;
 const gptimer_config_t color_timer_config = {
     .clk_src = GPTIMER_CLK_SRC_DEFAULT,
     .direction = GPTIMER_COUNT_UP,
-    .resolution_hz = freq_hz
+    .resolution_hz = FREQ_HZ
 };
 
 const gptimer_alarm_config_t color_alarm_config = {
@@ -92,14 +96,71 @@ void set_color(unsigned int new_color){
     ESP_ERROR_CHECK(gptimer_start(color_timer_handler));
 }
 
+void color_shift(){
+    color = color << 1;
+    if(color > 0xFFFFFF) color = 1;
+    set_color(color);
+}
+
+void color_rainbow(){
+    switch(color_rainbow_state){
+        case 0:
+            if(color == 1) color = 0;
+            else color += 0x100;
+            if(color == 0xFF00){
+                color_rainbow_state = 1;
+            }
+            break;
+        case 1:
+            color += 0x10000;
+            if(color == 0xFFFF00){
+                color_rainbow_state = 2;
+            }
+            break;
+        case 2:
+            color -= 0x100;
+            if(color == 0xFF0000){
+                color_rainbow_state = 3;
+            }
+            break;
+        case 3:
+            color += 1;
+            if(color == 0xFF00FF){
+                color_rainbow_state = 4;
+            }
+            break;
+        case 4:
+            color -= 0x10000;
+            if(color == 0xFF){
+                color_rainbow_state = 5;
+            }
+            break;
+        case 5:
+            color += 0x100;
+            if(color == 0xFFFF){
+                color_rainbow_state = 6;
+            }
+            break;
+        case 6:
+            color -= 1;
+            if(color == 0xFF00){
+                color_rainbow_state = 7;
+            }
+            break;
+        case 7:
+            color -= 0x100;
+            if(color == 0){
+                color_rainbow_state = 0;
+            }
+            break;
+    }
+    set_color(color);
+}
+
 void app_main() {
     init_led();
-    color = 1;
-    set_color(color);
     while(true){
-        vTaskDelay(5);
-        color = color << 1;
-        if(color > 0xFFFFFF) color = 1;
-        set_color(color);
+        color_rainbow();
+        vTaskDelay(1);
     };
 }

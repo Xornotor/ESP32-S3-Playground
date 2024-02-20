6b3227c45a2a76cbb57d7a00292bf3da8e790b6c
And Paiva <andre.pcr@hotmail.com>
2024-02-20 14:38:32 -0300

    Enhancement: Color update with queue

--- RGB-LED/src/main.c
+++ RGB-LED/src/main.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <math.h>
-#include <rom/ets_sys.h>
 #include "driver/ledc.h"
 #include "driver/gptimer.h"
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
+// Constantes de Configuração
 #define FREQ_HZ         40000
-#define DUTY_RESOLUTION 7
+#define DUTY_RESOLUTION LEDC_TIMER_7_BIT
+#define HPOINT          90
 const double duty_unit = (1e9/(FREQ_HZ * 128));
 const uint32_t LOW_DCYCLE = (uint32_t)(300/duty_unit);
 const uint32_t HIGH_DCYCLE = (uint32_t)(700/duty_unit);
 
-uint8_t color_weight = 74;
+// Variáveis globais para funções específicas
 unsigned int color = 0x000001;
-uint32_t color_duties[75];
 uint8_t color_rainbow_state = 0;
 
+//Protótipos de funções
 static bool change_color(gptimer_handle_t, const gptimer_alarm_event_data_t*, void*);
 void init_led();
 void set_color(unsigned int);
 void color_shift();
 void color_rainbow();
 
+// Handler de fila para gerenciamento de cor
+QueueHandle_t xQueueColor = NULL;
+
+// Configurações da controladora de LED
 const ledc_timer_config_t led_timer_config = {
     .speed_mode = LEDC_LOW_SPEED_MODE,
     .duty_resolution = DUTY_RESOLUTION,
@@ -39,9 +45,10 @@ const ledc_channel_config_t led_channel_config = {
     .intr_type = LEDC_INTR_DISABLE,
     .timer_sel = LEDC_TIMER_0,
     .duty = 0,
-    .hpoint = 90
+    .hpoint = HPOINT
 };
 
+// Configurações de timer para mudança de cores
 gptimer_handle_t color_timer_handler = NULL;
 
 const gptimer_config_t color_timer_config = {
@@ -60,6 +67,7 @@ const gptimer_event_callbacks_t color_cbs = {
     .on_alarm = change_color,
 };
 
+// Função de inicialização
 void init_led(){
     ESP_ERROR_CHECK(ledc_timer_config(&led_timer_config));
     ESP_ERROR_CHECK(ledc_channel_config(&led_channel_config));
@@ -67,41 +75,44 @@ void init_led(){
     ESP_ERROR_CHECK(gptimer_set_alarm_action(color_timer_handler, &color_alarm_config));
     ESP_ERROR_CHECK(gptimer_register_event_callbacks(color_timer_handler, &color_cbs, NULL));
     ESP_ERROR_CHECK(gptimer_enable(color_timer_handler));
+    xQueueColor = xQueueCreate(225, sizeof(uint16_t));
     ledc_timer_resume(LEDC_LOW_SPEED_MODE, LEDC_TIMER_0);
-    color_duties[0] = 0;
 }
 
+// Função de geração de pulsos para mudança de cor (Não chamar diretamente)
 static bool change_color(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx){
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, color_duties[color_weight]);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-    if(color_weight == 0){
-        color_weight = 74;
+    uint16_t d_cycle = 0;
+    if(xQueueReceive(xQueueColor, &d_cycle, 0) == errQUEUE_EMPTY){
         gptimer_stop(timer);
-        return true;
     }
-    color_weight--;
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, d_cycle);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
     return true;
 }
 
-//0x000000 to 0xffffff (GRB)
+// Função de seleção de cor - 0x000000 to 0xffffff (GRB)
 void set_color(unsigned int new_color){
-    int i;
+    uint8_t i;
+    uint16_t color_duty = 0;
     for(i = 0; i < 75; i++){
-        if(i <= 50){
-            color_duties[i] = 0;
-        }else{
-            color_duties[i] = (bool)((new_color >> (i-51)) & 1) ? HIGH_DCYCLE : LOW_DCYCLE;
+        if(i < 24){
+            color_duty = (bool)((new_color >> (23-i)) & 1) ? HIGH_DCYCLE : LOW_DCYCLE;
+        }
+        if(xQueueSendToBack(xQueueColor, &color_duty, 0) == errQUEUE_FULL){
+            printf("WARNING: QUEUE FULL");
         }
     }
     ESP_ERROR_CHECK(gptimer_start(color_timer_handler));
 }
 
+// Função de color shift (feita para rodar em loop)
 void color_shift(){
     color = color << 1;
     if(color > 0xFFFFFF) color = 1;
     set_color(color);
 }
 
+// Função de color rainbow (feita para rodar em loop)
 void color_rainbow(){
     switch(color_rainbow_state){
         case 0:
@@ -157,10 +168,11 @@ void color_rainbow(){
     set_color(color);
 }
 
+// Entry point de execução
 void app_main() {
     init_led();
     while(true){
         color_rainbow();
-        vTaskDelay(1);
+        vTaskDelay(2);
     };
 }

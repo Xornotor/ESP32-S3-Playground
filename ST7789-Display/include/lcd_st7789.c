31f64742475b8bdbd347a4b74e5001d3c3c2c876
And Paiva <andre.pcr@hotmail.com>
2024-07-11 23:22:30 -0300

    LCD Color Display com Funções de write string

---
+++ ST7789-Display/include/lcd_st7789.c
@@ -0,0 +1,178 @@
+#include <driver/spi_master.h>
+#include <driver/gpio.h>
+#include <esp_lcd_types.h>
+#include <esp_lcd_panel_io.h>
+#include <esp_lcd_panel_vendor.h>
+#include <esp_lcd_panel_ops.h>
+#include <esp_err.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
+#include <fonts.c>
+
+#define     LCD_HOST    SPI2_HOST
+
+#define     SCLK_PIN    12
+#define     MOSI_PIN    11
+#define     MISO_PIN    13
+#define     RST_PIN     15
+#define     DC_PIN      16 
+
+static const spi_bus_config_t buscfg = {
+        .sclk_io_num = SCLK_PIN,
+        .mosi_io_num = MOSI_PIN,
+        .miso_io_num = MISO_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 240 * 240 * sizeof(uint16_t),
+};
+
+static esp_lcd_panel_io_handle_t io_handle = NULL;
+
+static const esp_lcd_panel_io_spi_config_t io_config = {
+    .dc_gpio_num = DC_PIN,
+    .cs_gpio_num = -1,
+    .pclk_hz = 25 * 1000 * 1000,
+    .spi_mode = 3,
+    .trans_queue_depth = 10,
+    .lcd_cmd_bits = 8,
+    .lcd_param_bits = 8,
+};
+
+static esp_lcd_panel_handle_t panel_handle = NULL;
+
+static const esp_lcd_panel_dev_config_t panel_config = {
+    .reset_gpio_num = RST_PIN,
+    .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+    .bits_per_pixel = 16,
+};
+
+esp_err_t st7789_update_frame(uint16_t *f_buffer){
+    ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, 240, 240, (const void*) f_buffer));
+    return ESP_OK;
+}
+
+esp_err_t st7789_init() {
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+    return ESP_OK;
+}
+
+esp_err_t st7789_teste(uint16_t *f_buffer){
+    uint16_t color = 0x0000;
+    uint16_t i, j;
+
+    while(true){
+        for(i = 0; i < 240; i++){
+            for(j = 0; j < 240; j++){
+                f_buffer[240*i + j] = color;
+            }
+            //color += 0x1;
+        }
+        color += 0xF;
+        ESP_ERROR_CHECK(st7789_update_frame(f_buffer));
+        vTaskDelay(pdMS_TO_TICKS(33));
+        if(color >= 0xFFFF) break;
+    }
+    return ESP_OK;
+}
+
+esp_err_t st7789_color_lines(uint16_t *f_buffer){
+    uint16_t color = 0x0000;
+    uint16_t i, j;
+    for(i = 0; i < 240; i++){
+        for(j = 0; j < 240; j++){
+            f_buffer[240*i + j] = color;
+        }
+        color += 0x1;
+    }
+    return ESP_OK;
+}
+
+void st7789_write_char(uint16_t *f_buffer,
+              char c, 
+              uint16_t x,
+              uint16_t y,
+              uint16_t font_color,
+              uint8_t font_size
+              ){
+
+    uint16_t width, height, line;
+    uint8_t i, j;
+    uint16_t char_lines[26];
+
+    switch(font_size){
+        case 1:
+            width = 7;
+            height = 9;
+            for(i = 0; i < height; i++)
+                char_lines[i] = Font7x9[(height*(uint16_t) (c-32)) + i];
+            break;
+        case 2:
+            width = 11;
+            height = 18;
+            for(i = 0; i < height; i++)
+                char_lines[i] = Font11x18[(height*(uint16_t) (c-32)) + i];
+            break;
+        case 3:
+            width = 16;
+            height = 26;
+            for(i = 0; i < height; i++)
+                char_lines[i] = Font16x26[(height*(uint16_t) (c-32)) + i];
+            break;
+        default:
+            width = 6;
+            height = 8;
+            for(i = 0; i < height; i++)
+                char_lines[i] = Font6x8[(height*(uint16_t) (c-32)) + i];
+            break; 
+    }
+
+    for(i = y; i < y+height; i++){
+        line = char_lines[i-y] >> (16-width);
+        for(j = x; j < x+width; j++){
+            if((line & ((uint16_t) 1 << ((width-1) - (j-x)))) > 0)
+                f_buffer[j + (240*i)] = font_color;
+        }
+    }    
+}
+
+void st7789_print(uint16_t *f_buffer,
+              const char* string, 
+              uint16_t x,
+              uint16_t y,
+              uint16_t font_color,
+              uint8_t font_size
+              ){
+
+    uint16_t width, height;
+
+    switch(font_size){
+        case 1:
+            width = 7; height = 9; break;
+        case 2:
+            width = 11; height = 18; break;
+        case 3:
+            width = 16; height = 26; break;
+        default:
+            width = 6; height = 8; break; 
+    }
+    
+    uint16_t x_pos = x, y_pos = y;
+    char c;
+    while(*string){
+        c = *string;
+        if(c == '\n'){
+            x_pos = x;
+            y_pos += height;
+        }else{
+            st7789_write_char(f_buffer, c, x_pos, y_pos, font_color, font_size);
+            x_pos += width;
+        }
+        string += sizeof(char);
+    }
+}
--- ST7789-Display/src/main.c
+++ ST7789-Display/src/main.c
@@ -1,96 +1,24 @@
-#include <driver/spi_master.h>
-#include <driver/gpio.h>
-#include <esp_lcd_types.h>
-#include <esp_lcd_panel_io.h>
-#include <esp_lcd_panel_vendor.h>
-#include <esp_lcd_panel_ops.h>
-#include <esp_err.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <xornotor.c>
+#include <freertos/semphr.h>
+#include <esp_log.h>
+#include <esp_err.h>
+#include <lcd_st7789.c>
 
-#define     LCD_HOST    SPI2_HOST
-
-#define     SCLK_PIN    12
-#define     MOSI_PIN    11
-#define     MISO_PIN    13
-#define     RST_PIN     15
-#define     DC_PIN      16  
-#define     BLK_PIN     7
+uint16_t frame_buffer[240*240];
 
 void app_main() {
-    ESP_ERROR_CHECK(gpio_set_direction(BLK_PIN, GPIO_MODE_OUTPUT));
-    ESP_ERROR_CHECK(gpio_set_level(BLK_PIN, 1));
-
-    spi_bus_config_t buscfg = {
-        .sclk_io_num = SCLK_PIN,
-        .mosi_io_num = MOSI_PIN,
-        .miso_io_num = MISO_PIN,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = 240 * 40 * sizeof(uint16_t),
-    };
-
-    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
-
-    esp_lcd_panel_io_handle_t io_handle = NULL;
-    esp_lcd_panel_io_spi_config_t io_config = {
-        .dc_gpio_num = DC_PIN,
-        .cs_gpio_num = -1,
-        .pclk_hz = 16 * 1000 * 1000,
-        .spi_mode = 3,
-        .trans_queue_depth = 10,
-        .lcd_cmd_bits = 8,
-        .lcd_param_bits = 8,
-    };
+    ESP_ERROR_CHECK(st7789_init());
     
-    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
-
-    esp_lcd_panel_handle_t panel_handle = NULL;
-    esp_lcd_panel_dev_config_t panel_config = {
-        .reset_gpio_num = RST_PIN,
-        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
-        .bits_per_pixel = 16,
-    };
-
-    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
-
-    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
-    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
-    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
-
-    uint16_t color[10][10];
-
-    int i, j;
-    for(i = 0; i < 10; i++){
-        for(j = 0; j < 10; j++){
-            color[i][j] = 0x0000;
-        }
-    }
-
-    uint8_t x = 0, y = 0;
-
-    ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, 240, 240, (const void*) xornotor_map));
-
-    vTaskDelay(pdMS_TO_TICKS(3000));
+    //st7789_color_lines(frame_buffer);    
+    st7789_print(frame_buffer, "Isto e um teste", 0, 0, 0xFFFF, 0);
+    st7789_print(frame_buffer, "Isto e um teste", 0, 20, 0xFFFF, 1);
+    st7789_print(frame_buffer, "Isto e um teste", 0, 40, 0xFFFF, 2);
+    st7789_print(frame_buffer, "Isto e um teste", 0, 60, 0xFFFF, 3);
 
     while(true){
-        //ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
-        ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, x, y, x+10, y+10, (const void*) color));
-        x += 10;
-        if (x > 240) {
-            x = 0;
-            y += 10;
-        }
-        if (y > 240) {
-            x = 0;
-            y = 0;
-            for(i = 0; i < 10; i++){
-                for(j = 0; j < 10; j++){
-                    color[i][j] += 0xF;
-                }
-            }
-        }
-        vTaskDelay(pdMS_TO_TICKS(1));
+        st7789_update_frame(frame_buffer);
+        vTaskDelay(pdMS_TO_TICKS(33));
     }
+
 }
